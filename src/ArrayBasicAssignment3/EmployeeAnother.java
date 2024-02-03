d75d65641b097d5fef9e0805a92f9dedaea587c8
pBhuse <pratikshabhuse2002@gmail.com>
2024-02-03 15:25:11 +0530

    Write a program to Add Employee Class Object in Array, return it and print it
    Write a program to Add Employee Class Object in Array, return it and print it

---
+++ src/ArrayBasicAssignment3/EmployeeAnother.java
@@ -0,0 +1,18 @@
+package ArrayBasicAssignment3;
+
+public class EmployeeAnother {
+	//Employee obj=new Employee();
+	
+	void printArray(Employee[]arr)
+	{
+		System.out.println("......................................................................................");
+		System.out.println("Employee Records In EmployeeAnother class: ");
+		for (Employee emp : arr) {
+			
+			System.out.println(emp.toString());
+		}
+		
+	}
+
+
+}

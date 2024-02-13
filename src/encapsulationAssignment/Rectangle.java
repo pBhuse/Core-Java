a1af19330bbe64de796633ce00da73f1f7379373
pBhuse <pratikshabhuse2002@gmail.com>
2024-02-13 20:45:53 +0530

    Implementation of Encapsulation
    Implementation of Encapsulation

---
+++ src/encapsulationAssignment/Rectangle.java
@@ -0,0 +1,32 @@
+package encapsulationAssignment;
+
+public class Rectangle
+{
+	//Problem :- Design A Encapsulated Rectangle Class that should work for below method(getter and setter method)?
+
+	private int Length;
+	  private int Breadth;
+	
+	public int getLength() {
+		return Length;
+	}
+	public void setLength(int length) {
+		this.Length = length;
+	}
+	public int getBreadth() {
+		return Breadth;
+	}
+	public void setBreadth(int breadth) {
+		this.Breadth = breadth;
+	}
+	public Rectangle(int lth,int bth)
+	{
+		
+		Length=lth;
+		Breadth=bth;
+	   int Area=Length*Breadth;
+	   System.out.println("Area: " +Area); 
+
+	}
+
+}
---
+++ src/encapsulationAssignment/RectangleTest.java
@@ -0,0 +1,18 @@
+package encapsulationAssignment;
+
+public class RectangleTest
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("**** Implementation of Encapsulation ****");
+		Rectangle rt= new Rectangle(20,30);
+        rt.setLength(50);
+        rt.setBreadth(60);
+        int p=rt.getLength();
+        int q=rt.getBreadth();
+        int newArea=p*q;
+        System.out.println("New Area :"+newArea);
+	}
+  
+}

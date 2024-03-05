ac05d05bce504761d9d608f84c351eec8d5deb9d
pBhuse <pratikshabhuse2002@gmail.com>
2024-03-05 14:23:50 +0530

    overloading Main method
    overloading Main method

---
+++ src/mainMethodOverloaded/MainMethodOverloaded.java
@@ -0,0 +1,15 @@
+package mainMethodOverloaded;
+
+public class MainMethodOverloaded
+{
+	public static void main(String[] args)
+	{
+		MainMethodOverloaded mob=new MainMethodOverloaded();
+		mob.main();
+	}
+	public static void main()
+	{
+		System.out.println("Overloaded main()invoked");
+	}
+
+}

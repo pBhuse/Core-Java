e39380f1a8525bc8395122690f68d60872135dbc
pBhuse <pratikshabhuse2002@gmail.com>
2024-03-29 19:01:27 +0530

    To convert HashSet to Array
    To convert HashSet  to Array

---
+++ src/collectionSetAssignment/HashSetToArray.java
@@ -0,0 +1,29 @@
+package collectionSetAssignment;
+
+import java.util.HashSet;
+
+public class HashSetToArray {
+
+	public static void main(String[] args)
+	{
+     System.out.println("3] Write a program to convert HashSet to Array?");
+     HashSet<Integer>hs=new HashSet<Integer>();
+     hs.add(10);
+     hs.add(20);
+     hs.add(30);
+     hs.add(40);
+     hs.add(50);
+     System.out.println("Set Elements are"+hs);
+     //int[]arr=new int[hs.size()];
+     Integer[] intArray= hs.toArray(new Integer[hs.size()]);
+     hs.toArray(intArray);
+     System.out.println("Array Elements are :");
+
+     for (Integer integer : intArray) 
+     {
+		System.out.println(integer);
+	}
+     
+	}
+
+}

7ba2bc8c086d301e0caf0b5951c0ef1f6fbef04b
pBhuse <pratikshabhuse2002@gmail.com>
2024-03-31 17:34:17 +0530

    To get the first and last elements in a tree set.
    To get the first and last elements in a tree set.

---
+++ src/collectionSetAssignment/FirstAndLastInSet.java
@@ -0,0 +1,26 @@
+package collectionSetAssignment;
+
+import java.util.TreeSet;
+
+public class FirstAndLastInSet {
+
+	public static void main(String[] args)
+	{
+System.out.println("5] Write a Java program to get the first and last elements in a tree set.");
+	TreeSet<String>ts=new TreeSet<String>();
+	ts.add("Pratiksha");
+	ts.add("Smita");
+	ts.add("Reshma");
+	ts.add("Sanmesh");
+	System.out.println("Tree Set Elements :"+ts);
+	// Find first element of the tree set
+    Object first_element =  ts.first();
+
+	System.out.println("First Element  :"+first_element);
+	// Find last element of the tree set
+
+	Object last_element=ts.last();
+	System.out.println("Last Element :"+last_element);
+	}
+
+}
